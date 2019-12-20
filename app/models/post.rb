cd92ffc4eaa7014de9996cc57cfd2890a3d78dba
Victoria Hale <vic.hale74@gmail.com>
2019-12-20 07:20:58 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,6 @@ class Post < ActiveRecord::Base
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
       self.categories << category
-
     end
   end
 
